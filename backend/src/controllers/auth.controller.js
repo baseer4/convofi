4687701ae462f699c39607740cb1b4aeb51a7488
baseer4 <sbaseer003@gmail.com>
2025-04-20 12:47:32 +0530

    Added login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -47,10 +47,41 @@ export const signup = async (req,res)=>{
     }
 }
 
-export const login = (req,res)=>{
-    res.send("login route")
+
+
+//Login logic
+export const login = async (req,res)=>{
+    const { email,password } = req.body;
+    try {
+        const user = await User.findOne({email});
+        
+        if(!user){
+            return res.status(400).json({messasge:"Invalid credentials!"});
+        }
+       const ispasscorrect = await bcrypt.compare(password,user.password);
+       if(!ispasscorrect){
+             return res.status(400).json({messasge:"Invalid credentials!"});
+       }
+       generateToken(user._id,res)
+
+       res.status(200).json({
+        _id:user._id,
+        fullname:user.fullName,
+        email:user.email,
+        profilePic:user.profilePic,
+    });
+    } catch (error) {
+        console.log("Error during login",error.message);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 
 export const logout = (req,res)=>{
-    res.send("logout route")
+    try{
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message: "Logged out successfully"});
+    }catch(error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message : "Internal server error"});
+    }
 }
