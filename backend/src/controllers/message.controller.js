47a0d24ce3fff94d737c8320dcb70b43a29806b0
baseer4 <sbaseer003@gmail.com>
2025-04-27 14:36:01 +0530

    fix minor typo

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -49,7 +49,7 @@ export const sendMessage =async(req,res) => {
             senderId,
             receiverId,
             text,
-            image:imageurl,
+            image: imageUrl,
         });
 
         await  newMessage.save();
