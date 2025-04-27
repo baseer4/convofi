cfdd37e1d56ce56209f1502879166603d3c17466
baseer4 <sbaseer003@gmail.com>
2025-04-27 14:35:40 +0530

    add sidebar and homepage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -9,7 +9,9 @@ export const useAuthStore = create((set)=> ({
     isLoggingIn:false,
     isUpdatingProfile:false,
     
+    
     isCheckingAuth:true,
+    onlineUsers:[],
 
     checkAuth: async() =>{
         try {
