b3cda37c82597011ec12bff39d90b18112807c84
baseer4 <sbaseer003@gmail.com>
2025-06-28 15:09:19 +0530

    Add socket implementation for real time meessages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -5,16 +5,26 @@ import ChatHeader from './ChatHeader';
 import MessageSkeleton from './skeletons/MessageSkeleton';
 import { useAuthStore } from '../store/useAuthStore';
 import {formatMessageTime} from '../lib/utlis.js'
+import { useRef } from 'react';
+
 
 
 const ChatContainer = () => {
-  const {messages,getMessages,isMessagesLoading,selectedUser} =useChatStore()
+  const {messages,getMessages,isMessagesLoading,selectedUser,subscribeToMessages,unsubscribeFromMessages} =useChatStore()
   const {authUser} = useAuthStore();
+  const messageEndRef = useRef(null);
   useEffect(() => {
       getMessages(selectedUser._id);
+      subscribeToMessages();
+
+      return () => unsubscribeFromMessages();
     
-  }, [selectedUser?._id, getMessages]);
+  }, [selectedUser?._id, getMessages,subscribeToMessages,unsubscribeFromMessages]);
   
+useEffect(() => {
+  if(messageEndRef.current && messages)
+  messageEndRef.current.scrollIntoView({behavior : "smooth"})
+}, [messages]);
 
 
   if(isMessagesLoading) {
@@ -37,6 +47,7 @@ const ChatContainer = () => {
           <div
             key={message._id}
             className={`chat ${message.senderId== authUser._id  ? "chat-end": "chat-start"}`}
+            ref = {messageEndRef}
             >
             <div className=" chat-image avatar">
             <div className="size-10 rounded-full border">
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,8 @@
 import {create} from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { Socket } from "socket.io-client";
+import { useAuthStore } from "./useAuthStore";
 
 
 export const useChatStore = create((set,get) => ({
@@ -45,6 +47,26 @@ export const useChatStore = create((set,get) => ({
             toast.error(error.response.data.message);
         }
     },
+
+    subscribeToMessages:() =>{
+        const{selectedUser} = get()
+        if(!selectedUser) return;
+
+        const socket = useAuthStore.getState().socket;
+
+    
+        socket.on("newMessage", (newMessage) =>{ 
+            if(newMessage.senderId !== selectedUser._id) return;
+            set({
+                messages:[...get().messages,newMessage]
+            })
+        })
+    },
+
+    unsubscribeFromMessages: () =>{
+        const socket  = useAuthStore.getState().socket;
+        socket.off("newMessage")
+    } ,
 
     setSelectedUser: (selectedUser) => set({selectedUser}),
     
