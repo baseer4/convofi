768c9f396eade4523bc5805ab0c89c9d101ce5f9
baseer4 <sbaseer003@gmail.com>
2025-07-07 01:20:13 +0530

    Increase upload limit for profile upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,10 @@ dotenv.config();
 
 const PORT = process.env.PORT
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
+
 
 app.use(cors({
     origin: "http://localhost:5173",
